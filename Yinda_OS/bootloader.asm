e59699e4fe726be8601e2e84fa1ee50cb585c1c6
Josias <jkkanyinda@gmail.com>
2022-12-29 18:56:44 -0500

    Add files via upload
    Organized a bit.
    Put the print function in print.asm instead of bootloader.asm
    OS is now reading from memory/a disk

--- Yinda_OS/bootloader.asm
+++ Yinda_OS/bootloader.asm
@@ -1,28 +1,19 @@
 
 [org 0x7c00]
 
+mov [BOOT_DISK], dl
+
 mov bp, 0x7c00
 mov sp, bp
 
-mov bx, TestString
-call PrintString
+call ReadDisk
+
+jmp PROGRAM_SPACE
 
 jmp $
 
-PrintString:
-	mov ah, 0x0e
-	.Loop:
-	cmp [bx], byte 0
-	je .Exit
-		mov al, [bx]
-		int 0x10
-		inc bx
-		jmp .Loop
-	.Exit:
-	ret
-
-TestString:
-	db 'This is a Test String',0
+%include "print.asm"
+%include "diskread.asm"
 
 times 510-($-$$) db 0
 
